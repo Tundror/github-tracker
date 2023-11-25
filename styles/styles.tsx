573904d44d4846d02c87695082b287393e73e7e9
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-11-24 23:13:47 -0300

    feat: initial architecture

---
+++ styles/styles.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components/native";
+
+export const StyledContainer = styled.View`
+    flex: 1;
+    background-color: black;
+    align-items: stretch;
+    justify-content: flex-start;
+    position: relative;
+`;
+
+export const PageText = styled.Text`
+    color: white;
+    margin-top: 20px;
+    flex: 1;
+    text-align: center;
+`;
+
+export const Header = styled.View`
+    background-color: #222222;
+    padding: 0px;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 10%;
+`
+
+export const LogoImage = styled.Image`
+    height: 70px;
+    width: 70px;
+    margin-right: 10px;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
